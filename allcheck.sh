8717674ed3c628a3e82d436b2ded8f743e32de5d
kumagai <iariro@gol.com>
2021-11-19 20:08:03 +0900

    パス修正

--- allcheck.sh
+++ allcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-DIR=/home/pi/doc/private/python/youtube
+DIR=/home/pi/doc/private/python/youtube/
 
 python3 ${DIR}youtubecomment.py ${DIR}dragonball.json ${DIR}game.json ${DIR}music.json ${DIR}seiyuu.json ${DIR}etc.json
