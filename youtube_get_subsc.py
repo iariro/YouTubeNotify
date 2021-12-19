1b0cf43c1b2625fac572af5558e7303b74e02fd1
kumagai <iariro@gol.com>
2021-12-20 07:50:47 +0900

    チャンネルリストを外部ファイル化

--- youtube_get_subsc.py
+++ youtube_get_subsc.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
+import csv
 import ambient
 from googleapiclient.discovery import build
 
 YOUTUBE_API_KEY = 'AIzaSyBuV44B4RZq90SnDs_GvCz7zXwrR34ixXI'
-
-channels = ['UCVD_BTWC0dmWPZOWagpEeiA', # 'k k': 
-            'UC402okxWzbtUDIjKD4EQBsQ', # 'Ken Drumschool': 
-            'UCzTC_3yTbvyZLkUp1FxdMDA'] # 'rocky music': 
 
 def youtube_channel_detail(channel_id, api_key):
     api_service_name = 'youtube'
@@ -20,10 +17,19 @@ def youtube_channel_detail(channel_id, api_key):
 
     return search_response['items'][0]
 
+def read_channels_from_csv(file_path):
+    channels = []
+    with open(file_path) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            channels.append(row[0])
+
+    return channels
 
 if __name__ == '__main__':
     cnts = {}
     ambi = ambient.Ambient(44031, 'e857a6cd408e29b5')
+    channels = read_channels_from_csv('/home/pi/doc/private/python/youtube/youtube_get_subsc_list.txt')
     for i, channel_id in enumerate(channels):
         d = youtube_channel_detail(channel_id, YOUTUBE_API_KEY)
         cnts['d{}'.format(i + 1)] = d['statistics']['subscriberCount']
