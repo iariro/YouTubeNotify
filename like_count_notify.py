d5629cf84ff70ebf78cb6c0fc4f374125b88339e
kumagai <iariro@gol.com>
2022-04-11 05:24:58 +0900

    視聴数も表示するように

--- like_count_notify.py
+++ like_count_notify.py
@@ -23,10 +23,10 @@ def get_authenticated_service(developerKey):
         api_service_name, api_version, developerKey=developerKey)
 
 
-def get_uploads_playlist_id(youtube, channelId):
+def get_uploads_playlist_id(youtube, channel_id):
     request = youtube.channels().list(
         part="contentDetails",
-        id=channelId,
+        id=channel_id,
         fields="items/contentDetails/relatedPlaylists/uploads"
     )
     response = request.execute()
@@ -45,7 +45,7 @@ def get_video_id_in_playlist(youtube, playlistId):
 
     while request:
         response = request.execute()
-        video_id_list.extend(list(map(lambda item: item["snippet"]["resourceId"]["videoId"], response["items"])))
+        video_id_list.extend([item["snippet"]["resourceId"]["videoId"] for item in response["items"]])
         request = youtube.playlistItems().list_next(request, response)
 
     return video_id_list
@@ -64,18 +64,6 @@ def get_video_items(youtube, video_id_list):
         response = request.execute()
         video_items.extend(response["items"])
 
-    return video_items
-
-
-def get_image_url(video_item):
-    qualities = ['standard', 'high', 'medium', 'default']
-    for quality in qualities:
-        if quality in video_item['snippet']['thumbnails'].keys():
-            return video_item['snippet']['thumbnails'][quality]['url']
-    return ''
-
-
-def convertVideoItems(video_items):
     return {item["id"]: {
             'title': item["snippet"]["title"],
             'publishedAt': item["snippet"]["publishedAt"],
@@ -83,49 +71,49 @@ def convertVideoItems(video_items):
             'likes': int(item["statistics"]["likeCount"])} for item in video_items}
 
 
-def like_count_diff(json_file, channelId, regular):
-    video_items_json_old = None
+def like_count_diff(json_file, channel_id, regular):
+    video_items_old = None
     if os.path.isfile(json_file):
         with open(json_file, 'r') as f:
-            video_items_json_old = json.load(f)
+            video_items_old = json.load(f)
 
     youtube = get_authenticated_service('AIzaSyBuV44B4RZq90SnDs_GvCz7zXwrR34ixXI')
-    uploads_playlist_id = get_uploads_playlist_id(youtube, channelId)
+    uploads_playlist_id = get_uploads_playlist_id(youtube, channel_id)
     video_id_list = get_video_id_in_playlist(youtube, uploads_playlist_id)
     video_items = get_video_items(youtube, video_id_list)
-    video_items_json = convertVideoItems(video_items)
 
     if regular:
         with open(json_file, 'w') as f:
-            json.dump(video_items_json, f, indent=4, ensure_ascii=False)
+            json.dump(video_items, f, indent=4, ensure_ascii=False)
 
     diff_likes = []
     diff_views = []
-    if video_items_json_old:
-        for video_id, item in sorted(video_items_json.items(), key=lambda x: x[1]['publishedAt'], reverse=True):
-            if video_id in video_items_json_old:
-                if video_items_json[video_id]['likes'] != video_items_json_old[video_id]['likes']:
-                    diff_likes.append('{}：{}→{}'.format(video_items_json[video_id]['title'],
-                                      video_items_json_old[video_id]['likes'],
-                                      video_items_json[video_id]['likes']))
+    if video_items_old:
+        video_id_sorted = sorted(video_items.items(), key=lambda x: x[1]['publishedAt'], reverse=True)
+        for video_id, item in video_id_sorted:
+            if video_id in video_items_old:
+                if video_items[video_id]['likes'] != video_items_old[video_id]['likes']:
+                    diff_likes.append('{}：{}→{}'.format(video_items[video_id]['title'],
+                                      video_items_old[video_id]['likes'],
+                                      video_items[video_id]['likes']))
             else:
-                diff_likes.append('{}：{}'.format(video_items_json[video_id]['title'],
-                                  video_items_json[video_id]['likes']))
+                diff_likes.append('{}：{}'.format(video_items[video_id]['title'],
+                                  video_items[video_id]['likes']))
 
         view_total = 0
-        for video_id, item in sorted(video_items_json.items(), key=lambda x: x[1]['publishedAt'], reverse=True):
-            if video_id in video_items_json_old:
-                views_new = video_items_json[video_id]['views']
-                views_old = video_items_json_old[video_id]['views']
+        for video_id, item in video_id_sorted:
+            if video_id in video_items_old:
+                views_new = video_items[video_id]['views']
+                views_old = video_items_old[video_id]['views']
                 if views_new != views_old:
                     view_total += views_new - views_old
-                    diff_views.append('{}：{}→{}({})'.format(video_items_json[video_id]['title'],
+                    diff_views.append('{}：{}→{}({})'.format(video_items[video_id]['title'],
                                       views_old,
                                       views_new,
                                       views_new - views_old))
             else:
-                diff_views.append('{}：{}'.format(video_items_json[video_id]['title'],
-                                  video_items_json[video_id]['likes']))
+                diff_views.append('{}：{}'.format(video_items[video_id]['title'],
+                                  video_items[video_id]['likes']))
     return (diff_likes, diff_views, view_total)
 
 
@@ -139,8 +127,9 @@ def line_notify(message):
 
 if __name__ == "__main__":
     json_file = '/home/pi/doc/private/python/youtube/like_count.json'
+    channel_id = 'UCVD_BTWC0dmWPZOWagpEeiA'
     regular = len(sys.argv) == 1 or sys.argv[1] != '-peek'
-    (diff_likes, diff_views, view_total) = like_count_diff(json_file, 'UCVD_BTWC0dmWPZOWagpEeiA', regular)
+    (diff_likes, diff_views, view_total) = like_count_diff(json_file, channel_id, regular)
     message = None
     if len(diff_likes) > 0:
         message = "高評価：\n" + '\n'.join(diff_likes)
@@ -148,7 +137,7 @@ if __name__ == "__main__":
         if message is None:
             message = ""
         else:
-            message += "\n"
+            message += "\n\n"
         message += "視聴数：\n" + '\n'.join(diff_views)
     if message is not None:
         if regular:
